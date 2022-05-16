111098a2e4e612044049d016a0fbc318c0f3e2ce
Mohamed Asjadh <mohamedasjadh@gmail.com>
2022-05-16 09:32:26 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+// routes
+import Router from "./routes";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  );
+}
+export default App;
